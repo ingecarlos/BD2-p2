37d674e10f156036108356756573d56ef30a7d55
Carlos Aguilar <carlos.ricardo.crac@gmail.com>
2019-05-06 11:32:29 -0600

    setup mongo driver Full

--- laravelmongodbcrud/config/app.php
+++ laravelmongodbcrud/config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //MONGODB
+        Jenssegers\Mongodb\MongodbServiceProvider::Class,
+
     ],
 
     /*
